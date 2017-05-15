f24f559b80c415e9c46c9eed07beb991e591dfdc
Daniel Scholl <daniel@scholllopez.com>
2017-05-15 14:09:53 -0500

    Initial Code

---
+++ exports_sample.sh
@@ -0,0 +1,4 @@
+export NODE_ENV=production
+export PREFIX={unique_prefix}
+export DEPLOY_USER={deployment_user}
+export DEPLOY_PASSWORD={deployment_pwd}
---
+++ provision.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+source exports.sh
+
+if [[ $# -eq 0 ]] ; then
+    echo 'No Azure Site Specified'
+    exit 1
+fi
+
+JOB_DIR='wwwroot/App_Data/jobs/continuous/twitterbot'
+
+: ${DEPLOY_USER?"Need to set DEPLOY_USER"}
+: ${DEPLOY_PASSWORD?"Need to set DEPLOY_PASSWORD"}
+: ${PREFIX?"Need to set PREFIX"}
+
+repo="https://"
+repo+=$DEPLOY_USER
+repo+=":"
+repo+=$DEPLOY_PASSWORD
+repo+="@"
+repo+=$PREFIX
+repo+='-'
+repo+=$1
+
+if [ "$SLOT" ]; then
+	repo+='-'
+	repo+=$SLOT
+fi
+
+repo+='.scm.azurewebsites.net:443/'
+repo+=$PREFIX
+repo+='-'
+repo+=$1
+repo+='.git'
+
+echo 'Cloning Repository: '  $repo
+
+git clone $repo wwwroot
+
+if [ -d $JOB_DIR ]; then
+	rm -R $JOB_DIR/*
+fi
+
+cp package.json $JOB_DIR
+cp server.js $JOB_DIR
+cd $JOB_DIR
+npm install --production
+
+# cd azure
+# git add -A
+# git commit -m "Code Shipped"
+# git push origin master
